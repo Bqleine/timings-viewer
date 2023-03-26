72bd8462e76f90b7ce7b7833708228ebd84619ef
Noé Lopez <noe.lopez@epitech.eu>
2023-03-26 20:04:29 +0200

    add description

--- src/App.js
+++ src/App.js
@@ -15,20 +15,20 @@ class App extends Component {
     render() {
         return (
             <div>
-                <Navbar bg="light" expand="lg">
-                    <Container>
-                        <Navbar.Brand>Baleine's Timings Viewer</Navbar.Brand>
-                    </Container>
-                </Navbar>
-                <Container>
-                    <Form.Group className={"pt-3"}>
-                        <h3>Raw Timing</h3>
-                        <Form.Control as={"textarea"} size={"sm"} rows={10} onChange={(event) => this.updateState(event)}/>
-                    </Form.Group>
+            <Container>
+	    <br />
+            <h1>Baleine's Timing viewer</h1>
+	    <p>This is a website to display raw pocketmine generated timings as a table.</p>
+	    <p>Paste your timings into the text field to start or use <a href="https://timings.pmmp.io/?id=302455&raw=1" target="_blank">an example timing</a>.</p>
+	    <p>You can click on table headers to sort and on table rows to view details if available.</p>
+            <Form.Group className={"pt-3"}>
+            <h3>Raw Timing</h3>
+            <Form.Control as={"textarea"} size={"sm"} rows={10} onChange={(event) => this.updateState(event)}/>
+            </Form.Group>
 
-                    <h3 className={"pt-3"}>Formatted Timing</h3>
-                    <TimingsViewer timings={this.state.rawTiming}/>
-                </Container>
+            <h3 className={"pt-3"}>Formatted Timing</h3>
+            <TimingsViewer timings={this.state.rawTiming}/>
+            </Container>
             </div>
         );
     }
